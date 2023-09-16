0c12db4f3675526d3da8d29e3467a8b4db389bd2
Jarrod Goodney <jarrod.goodney@gmail.com>
2023-09-16 10:41:49 -0400

    11th. Set up Swiper properly

--- app/QuoteCarousel.js
+++ app/QuoteCarousel.js
@@ -1,11 +1,55 @@
+'use client'
 import React from 'react'
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
 import styles from './styles/quoteCarousel.module.css'
 
 export default function QuoteCarousel() {
   return (
-    <div>
-      
-    </div>
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation = {{
+        nextEl: styles.swiperBtnNext,
+        prevEl: styles.swiperBtnPrev
+      }}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      className={styles.quoteContainer}
+    >
+        <SwiperSlide className={styles.quoteSlide}>
+          <div className={styles.testimonialImage}></div>
+          <h4 className={styles.testimonialHeading}>Cost Care Health</h4>
+          <p className={styles.testimonialText}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis repudiandae vero ullam explicabo, at tenetur nobis quos voluptates hic odio accusantium alias, libero accusamus. Rerum.</p>
+        </SwiperSlide>
+        <SwiperSlide className={styles.quoteSlide}>
+          <div className={styles.testimonialImage}></div>
+          <h4 className={styles.testimonialHeading}>Business 2</h4>
+          <p className={styles.testimonialText}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis repudiandae vero ullam explicabo, at tenetur nobis quos voluptates hic odio accusantium alias, libero accusamus. Rerum.</p>
+        </SwiperSlide>
+        <SwiperSlide className={styles.quoteSlide}>
+          <div className={styles.testimonialImage}></div>
+          <h4 className={styles.testimonialHeading}>Business 3</h4>
+          <p className={styles.testimonialText}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis repudiandae vero ullam explicabo, at tenetur nobis quos voluptates hic odio accusantium alias, libero accusamus. Rerum.</p>
+        </SwiperSlide>
+        <SwiperSlide className={styles.quoteSlide}>
+          <div className={styles.testimonialImage}></div>
+          <h4 className={styles.testimonialHeading}>Business 4</h4>
+          <p className={styles.testimonialText}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis repudiandae vero ullam explicabo, at tenetur nobis quos voluptates hic odio accusantium alias, libero accusamus. Rerum.</p>
+        </SwiperSlide>
+
+    </Swiper>
   )
 }
